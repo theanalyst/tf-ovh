ffadf44d8fe01f37f271756b3a4e1b83ceca3f38
Abhishek Lekshmanan <abhishek@suse.com>
2016-12-07 17:46:18 +0100

    adding facility to create minions and self- bootstrap
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- ovh.tf
+++ ovh.tf
@@ -1,8 +1,32 @@
 provider "openstack" {}
 
+data "template_file" "master-bootstrap" {
+  template = "${file("master-bootstrap.tpl")}"
+  vars {
+    master_ip = "${var.master_ip}"
+  }
+}
+
 resource "openstack_compute_instance_v2" "salt-master" {
   count = "1"
-  name = "salt-master"
+  name = "${var.vm_name_prefix}-salt-master"
+  key_pair = "${var.key_pair}"
+  image_name = "${var.image_name}"
+  flavor_name = "${var.master_flavor}"
+  network  {
+    name = "Ext-Net"
+    access_network = true
+  }
+  network {
+    name = "VLAN"
+    fixed_ip_v4 = "${var.master_ip}"
+  }
+  user_data = "${data.template_file.master-bootstrap.rendered}"
+}
+
+resource "openstack_compute_instance_v2" "salt-minion" {
+  count = "1"
+  name = "${var.vm_name_prefix}-salt-master"
   key_pair = "${var.key_pair}"
   image_name = "${var.image_name}"
   flavor_name = "${var.master_flavor}"
@@ -13,5 +37,19 @@ resource "openstack_compute_instance_v2" "salt-master" {
   network {
     name = "VLAN"
   }
-
+  provisioner "remote-exec" {
+    inline = [
+      "zypper --quiet --non-interactive in salt-minion",
+      "echo \"master: ${var.master_ip}\" > /etc/salt/minion.d/minion.conf",
+      "ip addr add ${self.network.1.fixed_ip_v4}/24 dev eth1",
+      "ip link set eth1 up",
+      "systemctl enable salt-minion",
+      "systemctl start salt-minion"
+    ]
+    connection{
+      type = "ssh"
+      user = "${var.login_user}"
+      agent = true
+    }
+  }
 }
--- vars.tf
+++ vars.tf
@@ -2,8 +2,18 @@ variable "key_pair" {
   "default" = "abhi-keys"
 }
 variable "image_name"{
-  "default" = "sp2-reg2"
+  "default" = "ses4"
 }
 variable "master_flavor" {
   "default" = "vps-ssd-1"
+}
+variable "vm_name_prefix" {
+  "default" = "tf"
+}
+# this should be in the same subnet you create in vRACK
+variable "master_ip" {
+  "default" = "192.168.2.10"
+}
+variable "login_user" {
+  "default" = "root"
 }
